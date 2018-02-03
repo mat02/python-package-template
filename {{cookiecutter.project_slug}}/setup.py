fed27c0337e6c62adae50c0471670f812e51868f
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2018-02-03 11:59:28 +0000

    revert changes to setup.py

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -20,22 +20,9 @@ pfile = Project(chdir=False).parsed_pipfile
 default = convert_deps_to_pip(pfile['packages'], r=False)
 development = convert_deps_to_pip(pfile['dev-packages'], r=False)
 
-# extract dependency links
-dependency_links = []
-
-for dependencies in (default, development):
-    for index, dependency in enumerate(dependencies):
-        if '+' in dependency:
-            *_, dependency_link = dependency.split()
-            dependency_links.append(
-                dependency_link
-            )
-            dependencies.pop(index)
-
 setup(
     install_requires=default,
     tests_require=development,
-    dependency_links=dependency_links,
     extras_require={
         'dev': development,
         'development': development,
