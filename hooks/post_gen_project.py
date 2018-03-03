48b9b43003732e486a3fde78ea8d65ef61fe4616
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2018-03-02 18:54:52 -0800

    add git hook creation to cookiecutter

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from pathlib import Path
 import os
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
@@ -8,8 +9,23 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
+def install_hooks():
+    """
+    Install git hooks to make life easier.
+    """
+    hooks_path = Path('{{cookiecutter.project_slug}}/.git/hooks/')
+
+    os.makedirs(hooks_path, exist_ok=True)
+
+    pre_push_path = Path(hooks_path, 'pre-push')
+
+    pre_push_path.write_text('{{cookiecutter.project_slug}} dev test')
+
+
 if __name__ == '__main__':
 
     open_source = '{{ cookiecutter.open_source }}'.lower()
     if not open_source == 'y' or open_source == 'yes':
         remove_file('LICENSE')
+
+    install_hooks()
